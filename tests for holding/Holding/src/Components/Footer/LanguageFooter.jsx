92c6b92532b09c205319916b84df34284c6617f4
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:58:07 +0500

    Create LanguageFooter.jsx

---
+++ tests for holding/Holding/src/Components/Footer/LanguageFooter.jsx
@@ -0,0 +1,13 @@
+import './LanguageFooter.css'
+
+const LanguageFooter = () => {
+    return(
+        <div className='langFooter'>
+            <button className='grayBtnFooter'>Py</button>
+            <button className='whiteBtnFooter'>En</button>
+        </div>
+    )
+}
+
+
+export default LanguageFooter
