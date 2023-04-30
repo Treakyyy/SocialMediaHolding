06ea7d6065eae86f9033bff5411b5efffcd6bc66
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:59:38 +0500

    Create Language.jsx

---
+++ tests for holding/Holding/src/Components/Header/Language.jsx
@@ -0,0 +1,12 @@
+import './Language.css'
+
+const Language = () => {
+    return(
+        <div  className="lang">
+            <button className='blackLang'>En</button>
+            <button className='whiteLang'>Py</button>
+        </div>
+    )
+}
+
+export default Language
