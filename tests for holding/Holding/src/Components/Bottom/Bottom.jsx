1614d58c743c3021b2d318a79424ccd4108e572d
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:57:44 +0500

    Create Bottom.jsx

---
+++ tests for holding/Holding/src/Components/Bottom/Bottom.jsx
@@ -0,0 +1,14 @@
+import Footer from '../Footer/Footer'
+import Sleeping from '../Sleeping/Sleeping'
+import './Bottom.css'
+
+const Bottom = () => {
+    return(
+        <div className='bottomBackground'>
+            <Sleeping/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Bottom
