bce9250aa9ad72949386d169249867ede043c4ef
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:59:32 +0500

    Create Registration.jsx

---
+++ tests for holding/Holding/src/Components/Header/Registration.jsx
@@ -0,0 +1,12 @@
+import './Registration.css'
+
+const Registration = () => {
+    return(
+        <div className='register'>
+            <button className='signin'><img src="signin.png" alt="" /> Sign In</button>
+            <button className='gold'><img src="btngold.png" alt="" /> Sign Up</button>
+        </div>
+    )
+}
+
+export default Registration
