11bf31ce558cf76b963f005a8b5125c7e163a05d
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:58:17 +0500

    Create Connection.jsx

---
+++ tests for holding/Holding/src/Components/Header/Connection.jsx
@@ -0,0 +1,12 @@
+import './Connection.css'
+
+const Connection = () => {
+    return(
+        <div className='connection'>
+            <button className='blackConnect'><img className='imgConnect' src="telegram.png" alt="" /></button>
+            <button className='blackConnect'><img className='imgConnect' src="message.png" alt="" /></button>
+        </div>
+    )
+}
+
+export default Connection
