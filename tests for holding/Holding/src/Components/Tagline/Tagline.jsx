d5f4829d6305cb872ee166a29d9922ab9b1769c7
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:58:53 +0500

    Create Tagline.jsx

---
+++ tests for holding/Holding/src/Components/Tagline/Tagline.jsx
@@ -0,0 +1,16 @@
+import './Tagline.css'
+
+const Tagline = () => {
+    return(
+        <div className='tag'>
+            <h1 className='dash'>–</h1>
+            <div className='tagline'>
+                <p>Valorous</p> 
+                <p>Affiliate</p> 
+                <p>Program</p>
+            </div>
+        </div>
+    )
+}
+
+export default Tagline
