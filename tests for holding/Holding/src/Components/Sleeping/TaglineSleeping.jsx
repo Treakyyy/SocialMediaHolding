c982321897d4ea3320c58de34fc218373e854f77
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:58:48 +0500

    Create TaglineSleeping.jsx

---
+++ tests for holding/Holding/src/Components/Sleeping/TaglineSleeping.jsx
@@ -0,0 +1,11 @@
+import './TaglineSleeping.css'
+
+const TaglineSleeping = () => {
+    return(
+            <p className='tagline'>
+                We've been sleeping <br /> for the last 10 years <br />and now we're awake!
+            </p>
+    )
+}
+
+export default TaglineSleeping
