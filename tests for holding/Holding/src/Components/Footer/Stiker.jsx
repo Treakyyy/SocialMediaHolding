acc5d4e2b963eee717704c97dc604ca6b7b203c0
MihailSuvorov555 <128746742+Treakyyy@users.noreply.github.com>
2023-05-01 01:58:12 +0500

    Create Stiker.jsx

---
+++ tests for holding/Holding/src/Components/Footer/Stiker.jsx
@@ -0,0 +1,12 @@
+import './Stiker.css'
+
+const Stiker = () => {
+    return(
+        <div className='stikers'>
+            <img src="Vector (1).png" alt="" />
+            <img className='frame' src="Frame 25.png" alt="" />
+        </div>
+    )
+}
+
+export default Stiker
